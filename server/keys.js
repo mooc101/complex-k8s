e04295291db1d4462c0990d6e115a484e4991260
Sandesh Kumar <sandesh@moven.com>
2020-02-12 04:26:28 -0500

    Add k8s configs

--- server/keys.js
+++ server/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
   pgDb: process.env.PG_DB,
   pgHost: process.env.PG_HOST,
-  pgPass: process.env.PG_PASS,
+  pgPass: process.env.PGPASSWORD,
   pgPort: process.env.PG_PORT,
   pgUser: process.env.PG_USER,
   redisHost: process.env.REDIS_HOST,
