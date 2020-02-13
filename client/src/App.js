71cc28563652e3b02ff723c28f2bd29e3bcab289
Sandesh Kumar <sandesh@moven.com>
2020-02-12 20:40:14 -0500

    Change app version

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Fib Calculator v2</h1>
+          <h1 className="App-title">Fib Calculator K8S</h1>
           <Link to="/">Home</Link>
           <Link to="/newpage">New Page</Link>
         </header>
